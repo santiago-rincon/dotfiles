3880d48a2e03322a043257b0b1b9af857423378c
Cristian Santiago Rincón <114261510+santiago-rincon@users.noreply.github.com>
2024-04-21 15:27:13 -0500

    Update ethernet_status.sh

--- config/bin/ethernet_status.sh
+++ config/bin/ethernet_status.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-ip=$(/usr/bin/hostname -I | awk '{print $1}')
 if [ "$(nmcli | grep "conectado to" | wc -l)" = "0" ]; then
   echo "%{F#7dcfff}󰈂 %{F#ffffff}Desconectado%{u-}";
 elif [ "$(nmcli | grep "eth0: conectado to" | wc -l)" = "1" ]; then
-  timeout 1 ping 8.8.8.8 -c1 &>/dev/null && echo "%{F#7dcfff}󰈀 %{F#ffffff}"$ip"%{u-}" || echo "%{F#7dcfff}󰌙 %{F#ffffff}"$ip"%{u-}";
-else
-  timeout 1 ping 8.8.8.8 -c1 &>/dev/null && echo "%{F#7dcfff} %{F#ffffff}"$ip"%{u-}" || echo "%{F#7dcfff}󱚵 %{F#ffffff}"$ip"%{u-}";
+  ip=$(nmcli | grep "eth0: conectado" -A 5 | grep inet4 | awk '{print $NF}' | awk '{print $1}' FS=\/);
+  timeout 1 ping 8.8.8.8 -c1 &>/dev/null && echo "%{F#7dcfff}󰈀 %{F#ffffff}"$ip"%{u-}" || echo "%{F#ff1e12}󰌙 %{F#ffffff}"$ip"%{u-}"; 
+elif [ "$(nmcli | grep "usb0: conectado to" | wc -l)" = "1" ]; then
+  ip=$(nmcli | grep "usb0: conectado" -A 5 | grep inet4 | awk '{print $NF}' | awk '{print $1}' FS=\/);
+  timeout 1 ping 8.8.8.8 -c1 &>/dev/null && echo "%{F#7dcfff} %{F#ffffff}"$ip"%{u-}" || echo "%{F#ff1e12} %{F#ffffff}"$ip"%{u-}";
+elif [ "$(nmcli | grep "wlan0: conectado to" | wc -l)" = "1" ]; then
+  ip=$(nmcli | grep "wlan0: conectado" -A 5 | grep inet4 | awk '{print $NF}' | awk '{print $1}' FS=\/);
+  timeout 1 ping 8.8.8.8 -c1 &>/dev/null && echo "%{F#7dcfff}󰖩 %{F#ffffff}"$ip"%{u-}" || echo "%{F#ff1e12}󱚵 %{F#ffffff}"$ip"%{u-}";
 fi
